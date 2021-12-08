5e8b5a11e6f9e07cdada2ddf7843530c969c2905
Kurt Rinnert <kurtrinnert@cern.ch>
2021-12-08 17:26:52 +0100

    Add necessary keyword argument back in.

--- classification.py
+++ classification.py
@@ -90,7 +90,7 @@ class ClassifierNN(nn.Module):
 
         return x
 
-    def train(self):
+    def train(self, mode=True):
         super(ClassifierNN, self).train()
         self.inference_mode = False
 
